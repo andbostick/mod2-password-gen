96a79675658ba84d16e7c0477f665751331cd2ad
andrew bostick <andbostick@gmail.com>
2023-06-18 16:41:37 -0400

    function to generate password

--- script.js
+++ script.js
@@ -36,7 +36,33 @@ function includesSpecial() {
 }
 
 function generatePassword(num) {
- 
+  var lower = "abcdefghijklmnopqrstuvwxyz"
+  var captial = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var numbers = "0123456789";
+  var special = "!?-_<>"
+  var result = " "
+  var characters = ""
+  if (num < 8 || num > 128) {
+    alert("enter a number between 8 and 128");
+    return undefined;
+  }
+  characters += lower;
+  if (hasUpperCase) {
+     characters += captial;
+  }
+  if (hasNumbers) {
+   characters += numbers
+  }
+  if (hasSpecialLetter) {
+    characters += special
+  }
+  console.log(characters)
+  for (i = 0; i < num; i++){
+    console.log(result)
+    result += characters.charAt(Math.floor(Math.random() * characters.length))
+  }
+  
+  return result
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
