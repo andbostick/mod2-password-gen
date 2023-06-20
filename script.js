349c067413bfceb17801264b1d46ff21a28a44e8
andrew bostick <andbostick@gmail.com>
2023-06-19 21:03:23 -0400

    fixed a bug of password updating before getting user input

--- script.js
+++ script.js
@@ -7,19 +7,19 @@ var hasNumbers;
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword(numOfChars);
-  var passwordText = document.querySelector("#password");
-
   includesNumbers();
   includesSpecial();
   includesUpperCase();
   getNumOfChars();
-  
+  var password = generatePassword(numOfChars);
+  var passwordText = document.querySelector("#password");
+
   passwordText.value = password;
 }
-
+//gets an input number for password length
 function getNumOfChars() {
   numOfChars = parseInt(prompt("enter a number between 8 and 128"));
+  // check if number was between 8 and 128
   if (numOfChars < 8 || numOfChars > 128) {
     alert("enter a number between 8 and 128");
     numOfChars = parseInt(prompt("enter a number between 8 and 128"));
@@ -36,33 +36,33 @@ function includesSpecial() {
 }
 
 function generatePassword(num) {
-  var lower = "abcdefghijklmnopqrstuvwxyz"
-  var captial = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var lower = "abcdefghijklmnopqrstuvwxyz";
+  var captial = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var numbers = "0123456789";
-  var special = "!?-_<>"
-  var result = " "
-  var characters = ""
+  var special = "!?-_<>";
+  var result = " ";
+
   if (num < 8 || num > 128) {
-    alert("enter a number between 8 and 128");
-    return undefined;
+    return "Requirements not met, Please try again";
   }
-  characters += lower;
+  var characters = lower;
+
+  //checks for options for password requirements and updates a string to loop through
   if (hasUpperCase) {
-     characters += captial;
+    characters += captial;
   }
   if (hasNumbers) {
-   characters += numbers
+    characters += numbers;
   }
   if (hasSpecialLetter) {
-    characters += special
+    characters += special;
   }
-  console.log(characters)
-  for (i = 0; i < num; i++){
-    console.log(result)
-    result += characters.charAt(Math.floor(Math.random() * characters.length))
+
+  for (i = 0; i < num; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
   }
-  
-  return result
+
+  return result;
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
